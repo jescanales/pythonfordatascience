51e9a1273f1c4c2f6c85d435c76546b3734f5ac1
Jesús Canales <jcanales@vallegrande.edu.pe>
2024-06-18 03:21:45 +0000

    seguimos avanzando con las listas

---
+++ basic/03_lists.py
@@ -0,0 +1,25 @@
+# Elementos para lista
+abarrote = "arroz"
+prenda = "camisa"
+automovil = "camioneta"
+celular = "Xiaomi"
+computadora = "laptop"
+
+lista_elementos = [abarrote, prenda, automovil, celular, computadora]
+
+print(lista_elementos)
+
+# Personalizando listas, es decir, listas dentro de listas
+lista_elementos = [
+    ["Abarrote", abarrote], 
+    ["Prenda", prenda], 
+    ["Automóvil", automovil], 
+    ["Celular", celular], 
+    ["Computadora", computadora]
+    ]
+
+# Imprimir elementos de lista 
+print(lista_elementos)
+
+# Imprimir tipo de dato de variable
+print(type(lista_elementos))
---
+++ basic/04_list-items.py
@@ -0,0 +1,34 @@
+# Elementos para lista
+abarrote = "arroz"
+prenda = "camisa"
+automovil = "camioneta"
+celular = "Xiaomi"
+computadora = "laptop"
+
+# Lista de elementos 1
+lista_elementos = [
+    ["Abarrote", abarrote], 
+    ["Prenda", prenda], 
+    ["Automóvil", automovil], 
+    ["Celular", celular], 
+    ["Computadora", computadora]
+    ]
+
+# Imprimir lista de elementos 1
+print(lista_elementos)
+
+# Lista de elementos 2
+personas = ["Jesús", 1.70, "Zulema", 1.60, "Fernando", 1.75, "Ysabel", 1.68]
+
+# Imprimir lista de personas
+print(personas)
+
+# Obteniendo elemento de lista por su índice
+print(lista_elementos[2])
+print(personas[2])
+
+# Extrae a partir del tercer elemento hasta el quinto elemento
+print(lista_elementos[2:5])
+
+# Extrae apartir del quinto elemento hasta el sexto elemento
+print(personas[4:6])
