c9c689f977109de806e9546d7a0511eea161011f
Jesús Canales <jcanales@vallegrande.edu.pe>
2024-06-18 02:30:51 +0000

    Seguimos practicando

---
+++ basic/lists.py
@@ -0,0 +1,14 @@
+# Elementos para lista
+abarrote = "arroz"
+prenda = "camisa"
+automovil = "camioneta"
+celular = "Xiaomi"
+computadora = "laptop"
+
+lista_elementos = [abarrote, prenda, automovil, celular, computadora]
+
+print(lista_elementos)
+
+# Personalizando listas
+lista_elementos = ["Abarrote", abarrote, "Prenda", prenda, "Automóvil", automovil, "Celular", celular, "Computadora", computadora]
+print(lista_elementos)
---
+++ basic/variables.py
@@ -0,0 +1,29 @@
+# Crear variables edad con el valor 40 e imprimir
+edad = 40
+print(edad)
+
+# Crear variable nombres y apellidos e imprimir
+nombres = 'Jesús'
+apellidos = 'Canales'
+print(nombres + ' ' + apellidos)
+
+# Crear proceso de compra
+producto = 'zapatillas'
+precio_unitario = 250
+cantidad = 2
+sub_total = precio_unitario * cantidad
+igv = sub_total * 0.18
+total = sub_total + igv
+print(producto)
+print(precio_unitario)
+print(cantidad)
+print(sub_total)
+print(igv)
+print(total)
+
+print("El producto comprado es: " + producto)
+print("El precio unitario es: " + str(precio_unitario))
+print("La cantidad es: " + str(cantidad))
+print("El subtotal: " + str(sub_total))
+print("El igv es: " + str(igv))
+print("El total es: " + str(total))
