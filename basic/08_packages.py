31f5ad1e6b6e27afd1d0feff0bef9cbe15af67d4
Jesús Canales <jcanales@vallegrande.edu.pe>
2024-06-18 06:07:13 +0000

    Aun nos queda poco por terminar lo de paquetes

---
+++ basic/08_packages.py
@@ -0,0 +1,10 @@
+# Instalar paquetes
+# http://pip.readthedocs.org/en/stable/installing/
+# Descargar: get-pip.py
+# python3 get-pip.py
+# pip3 install numpy
+
+import numpy
+numpy.array([1, 2, 3])
+print(numpy.array([1, 2, 3]))
+
