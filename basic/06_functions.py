bf0f8acd9aa96dee18ecc01376297b0875162a2a
Jesús Canales <jcanales@vallegrande.edu.pe>
2024-06-18 05:12:31 +0000

    terminamos con funciones

---
+++ basic/06_functions.py
@@ -0,0 +1,40 @@
+# Lista de elementos numéricos
+numbers = [5, 10, 20, 8, 12, 30, 2, 81]
+print(numbers)
+
+# Obtener el valor máximo
+number_max = max(numbers)
+print(number_max)
+
+# Redondear número
+redondear = round(1.75, 1)
+print(redondear)
+
+# Obtener ayuda de una función
+# print(help(round))
+
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse = True)
+
+# Print out full_sorted
+print(full_sorted)
