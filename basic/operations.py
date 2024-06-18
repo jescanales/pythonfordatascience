c74212361dd1d59789c18c49417b29a29964dfc0
Jesús Canales <jcanales@vallegrande.edu.pe>
2024-06-18 02:23:39 +0000

    Primeros pasos con python

---
+++ basic/operations.py
@@ -0,0 +1,17 @@
+# Empezamos con un Hola mundo... en español
+print("hola mundo...")
+
+# Ejemplo para imprimir Hello World en inglés
+print("Hello World!")
+
+# Ejemplo para sumar valores
+print(5 + 5)
+
+# Ejemplo para multiplicar valores
+print(5 * 9)
+
+# Ejmplo para restar números
+print(10 - 5)
+
+# Ejemplo para dividir números
+print(50 / 2)
